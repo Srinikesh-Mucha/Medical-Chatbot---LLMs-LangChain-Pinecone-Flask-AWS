0776882e46e74e1db6fec0efd5dc0c99439ef681
Srinikesh Mucha <nikeshrao1318@gmail.com>
2026-07-15 18:10:08 -0500

    requirements added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="medi_chatbot",
+    version="0.1.0",
+    author="Srinikesh Mucha",
+    author_email="nikeshrao1318@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
